a6a5504a8abf1e3dd5739ef51a97a8d14e75fa04
dipali Prakash Kelkar <kelkardipali1@gmail.com>
2019-09-11 12:12:04 +0530

    [Dipali] Add. mongo database events

---
+++ BackEnd/Config/databaseConfig.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+var fs = require('fs')
+var config = fs.readFileSync(`${__dirname}/config.json`)
+var mongoData = JSON.parse(config);
+var databaseUrl = mongoData.development.url
+class mongoEvent{
+
+    mongoEvent() {
+
+        mongoose.connect(databaseUrl);
+        /**
+         * @description:for providing mongoose connection
+         */
+        mongoose.connection.on('connected', function () {
+            console.log("Mongoose default connection is open to ", databaseUrl);
+        });
+        /**
+         * @description:for providing error while mongoose connection
+         */
+        mongoose.connection.on('error', function (err) {
+            console.log("Mongoose default connection has occured " + err + " error");
+        });
+        /**
+         * @description:mongoose disconnected
+         */
+        mongoose.connection.on('disconnected', function () {
+            console.log("Mongoose default connection is disconnected");
+        });
+        /**
+         * @description:mongoose connection closed
+         */
+        process.on('SIGINT', function () {
+            mongoose.connection.close(function () {
+                console.log("Mongoose default connection is disconnected due to application termination");
+                process.exit(0)
+            });
+        });
+    }
+}
+var mongoEvents= new mongoEvent();
+module.exports = mongoEvents
