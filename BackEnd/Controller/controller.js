832354f94fcf3c6cdfdf5d595f422fe0c60c7212
dipali Prakash Kelkar <kelkardipali1@gmail.com>
2019-09-12 16:28:54 +0530

    [Dipali] Add. checking ability of login when two length password is passed

--- BackEnd/Controller/controller.js
+++ BackEnd/Controller/controller.js
@@ -1,5 +1,6 @@
 exports.login = (req, res) => {
     req.checkBody('email', 'Invalid email').isEmail().not().isEmpty();
+    req.checkBody("password", "password is not valid").isLength({ min: 3 })
     var validationError = req.validationErrors();
     var errorResponse= {};
     var successResponse={};
--- BackEnd/Test/loginTest.js
+++ BackEnd/Test/loginTest.js
@@ -4,6 +4,7 @@ var chaiHttp = require('chai-http')
 chai.use(chaiHttp);
 var server = require('../server');
 describe('Login Test', () => {
+
     it('GivenInvalidEmail_WhenValidate_ThenReturnStatusUnprocessableEntity', (done) => {
         this.timeout = 10000;
         userData = {
@@ -14,6 +15,7 @@ describe('Login Test', () => {
             done();
         })
     })
+
     it('GivenEmptyEmail_WhenValidate_ThenReturnStatusunprocessableEntity', (done) => {
         this.timeout = 10000;
         userData = {
@@ -24,4 +26,17 @@ describe('Login Test', () => {
             done();
         })
     })
+
+    it('GivenValidEmailAndPasswordWithTwoLength_WhenValidate_ThenReturnUnprocessableEntity', (done) => {
+        this.timeout = 10000;
+        userData = {
+            "email":"Dipali@gmail.com",
+            "password": "di"
+        }
+        chai.request(server).post('/login').send(userData).end((err, res) => {
+            assert.equal(422, res.status)
+            done();
+        })
+    })
+
 })
