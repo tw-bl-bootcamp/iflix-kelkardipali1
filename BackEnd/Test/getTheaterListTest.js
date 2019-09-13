279246f84b6cfa344a433729aae4971ffced78af
dipali Prakash Kelkar <kelkardipali1@gmail.com>
2019-09-13 12:29:22 +0530

    [Dipali] Add. checking capability if unavailable route passed return 404

---
+++ BackEnd/Test/getTheaterListTest.js
@@ -0,0 +1,14 @@
+var chai = require('chai')
+var assert = require('assert')
+var chaiHttp = require('chai-http')
+chai.use(chaiHttp);
+var server = require('../server');
+describe('getMovieList Test', () => {
+
+    it('GivenUnavailablePath_WhenFindTheaterList_ThenReturnNotFoundStatus', (done) => {
+        chai.request(server).get('/theater').end((err, res) => {
+            assert.equal(404, res.status)
+            done();
+        })
+    })
+})
